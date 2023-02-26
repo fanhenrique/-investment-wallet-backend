2dd62a67f89adc00570d398e7fa8628b3a8c2054
Henrique Fan <fanhenrique@gmail.com>
2023-02-26 16:54:58 -0300

    Update: django-cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -59,7 +59,13 @@ REST_FRAMEWORK = {
   'PAGE_SIZE': 8
 }
 
+#config django-cors-headers
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:5173/",
+]
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware", #django-cors-headers
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
